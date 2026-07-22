095b061aafbb54bacec78f5154ddae7b42747d92
Fer-Jg <76271548+Fer-Jg@users.noreply.github.com>
2026-07-22 00:25:12 -0600

    feat: add PDF soft dependency check

--- src/utils/soft-dependencies.ts
+++ src/utils/soft-dependencies.ts
@@ -1,16 +1,30 @@
 import * as vscode from 'vscode';
 import logger from './logging';
+import { NONAME } from 'dns/promises';
+
+/*
+Soft Dependencies:
+
+Any yaml:
+- redhat.vscode-yaml
+
+Any PDF:
+- tomoki1207.pdf
+- mathematic.vscode-pdf
+- analytic-signal.preview-pdf
+
+*/
 
 export function detectSoftDependencies() {
     const yamlExtension = vscode.extensions.getExtension("redhat.vscode-yaml");
-    
+
     logger.info("Checking for soft dependencies...");
 
     if (yamlExtension) {
         logger.info("YAML extension is installed - Good :)");
     } else {
         logger.warn("YAML extension is not installed - Bad :(");
-        
+
         vscode.window.showWarningMessage("YAML preview extension is not installed, it is recommended to have it for a better experience.", "View in Marketplace", "Dismiss").then(selection => {
             if (selection === "View in Marketplace") {
                 // Lead the user to the extension marketplace to let them install the YAML extension by themselves
@@ -18,6 +32,39 @@ export function detectSoftDependencies() {
             }
             if (selection === "Dismiss") {
                 vscode.window.showInformationMessage("You can install the YAML extension later from the Extensions view.");
+            }
+        });
+    }
+
+    const pdfExtensions = [
+        "tomoki1207.pdf",
+        "mathematic.vscode-pdf",
+        "analytic-signal.preview-pdf"
+    ];
+
+    let pdfExtensionFound = undefined;
+
+    for (const ext of pdfExtensions) {
+        const pdfExtension = vscode.extensions.getExtension(ext);
+        if (pdfExtension) {
+            pdfExtensionFound = pdfExtension;
+            break;
+        }
+    }
+
+    logger.info("Checking for soft dependencies...");
+
+    if (pdfExtensionFound !== undefined) {
+        logger.info(`PDF extension (${pdfExtensionFound.id}) is installed - Good :)`);
+    } else {
+        logger.warn("PDF extension is not installed - Bad :(");
+
+        vscode.window.showWarningMessage("PDF preview extension is not installed, it is recommended to have it for a better experience.", "View in Marketplace", "Dismiss").then(selection => {
+            if (selection === "View in Marketplace") {
+                vscode.commands.executeCommand("extension.open", "tomoki1207.pdf");
+            }
+            if (selection === "Dismiss") {
+                vscode.window.showInformationMessage("You can install the PDF extension later from the Extensions view.");
             }
         });
     }
