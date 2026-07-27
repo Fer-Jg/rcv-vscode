20a1a402e87a3417dd35c44861dcd387c3b07856
Fer-Jg <76271548+Fer-Jg@users.noreply.github.com>
2026-07-26 20:44:30 -0600

    test: improved clean instance script

--- test-clean-instance.cmd
+++ test-clean-instance.cmd
@@ -7,4 +7,44 @@ echo  VS Code Clean Extension Test Environment
 echo ==================================================
 echo.
 
-rmdir /s /q "%TEMP%\vscode-clean" 2>nul & rmdir /s /q "%TEMP%\vscode-clean-extensions" 2>nul & code --extensions-dir "%TEMP%\vscode-clean-extensions" --install-extension ".\rendercv-vscode-0.0.1.vsix" && code --user-data-dir "%TEMP%\vscode-clean" --extensions-dir "%TEMP%\vscode-clean-extensions" && color 07 && echo Done! && endlocal
+REM --------------------------------------------------
+REM Parse optional arguments
+REM Usage:
+REM   test-clean-instance.cmd
+REM   test-clean-instance.cmd --folder "C:\Projects\MyProject"
+REM --------------------------------------------------
+
+set "FOLDER="
+
+:parse
+if "%~1"=="" goto args_done
+
+if /I "%~1"=="--folder" (
+    set "FOLDER=%~2"
+    shift
+)
+
+shift
+goto parse
+
+:args_done
+
+echo Using workspace:
+echo   %FOLDER%
+echo.
+
+echo Cleaning previous test environment...
+rmdir /s /q "%TEMP%\vscode-clean" 2>nul
+rmdir /s /q "%TEMP%\vscode-clean-extensions" 2>nul
+
+echo Installing extension and launching VS Code...
+code ^
+    --extensions-dir "%TEMP%\vscode-clean-extensions" ^
+    --install-extension ".\rendercv-vscode-0.0.1.vsix" && ^
+code ^
+    --user-data-dir "%TEMP%\vscode-clean" ^
+    --extensions-dir "%TEMP%\vscode-clean-extensions" ^
+    "%FOLDER%" && ^
+color 07 && echo. && echo Done! && endlocal
+
+:end
